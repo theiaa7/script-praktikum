db1293293094c4e71fc25a07f37bcfb81dd7f203
Byan <ryan.alsty@gmail.com>
2022-10-19 20:07:36 +0700

    algoP3.cpp
    tes

---
+++ algoP3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a = 1;
+    int input;
+    char b = '2';
+    float c = 5.21;
+    double d = 7.9245232324;
+    bool e = true;
+    bool j = false;
+    string f = "bryan dan fakhriza";
+    auto g = 3;
+    unsigned int h = 2;
+    long i = 32432424242;
+    const float pi = 3.14;
+
+    cout << "int a awal adalah : " << a << endl;
+    a = 20;
+    cout << "int a baru adalah : " << a << endl;
+    cin >> input;
+    cout << "int input adalah : " << input << endl;
+    cout << "char b adalah : " << b << endl;
+    cout << "float C adalah : " << c << endl;
+    cout << "double d adalah : " << d << endl;
+    cout << "bool e adalah : " << e << endl;
+    cout << "bool j adalah : " << j << endl;
+    cout << "string f adalah : " << f << endl;
+    cout << "auto g adalah : " << g << endl;
+    cout << "unsign h adalah : " << h << endl;
+    cout << "long i adalah : " << i << endl;
+    cout << "const float pi adalah : " << pi << endl;
+
+    return 0;
+}
